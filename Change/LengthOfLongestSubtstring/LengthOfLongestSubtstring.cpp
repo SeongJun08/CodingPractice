d4145e4f1bf9ff58201b6979c3b9d02793248f83
SeongJun08 <ellajune1612@gmail.com>
2024-07-21 21:18:50 +0900

    Create LengthOfLongestSubtstring.cpp

---
+++ Change/LengthOfLongestSubtstring/LengthOfLongestSubtstring.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+int LengthOfLongestSubtstring(std::string s)
+{
+    int result = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        int count = 1;
+
+        while (s[i + 1] == s[i] + 1)
+        {
+            i++;
+            count++;
+        }
+
+        if (result < count) result = count;
+    }
+    return result;
+}
+
+int main()
+{
+    std::string s{ "abcabcabcaa" };
+    std::cout << LengthOfLongestSubtstring(s) << std::endl;
+}

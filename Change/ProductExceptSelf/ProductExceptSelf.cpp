75e7863b24bdf224e528163ecd19fda477037fcf
SeongJun08 <ellajune1612@gmail.com>
2024-07-29 14:01:14 +0900

    나를 제외한 원소들의 곱

--- Change/ProductExceptSelf/ProductExceptSelf.cpp
+++ Change/ProductExceptSelf/ProductExceptSelf.cpp
@@ -19,6 +19,85 @@ std::vector<int> ProductExceptSelf(std::vector<int> &nums)
     return answer;
 }
 
+std::vector<int> ProductExceptSelf1(const std::vector<int> &nums)
+{
+    int accum{ 1 };
+    int count{};
+    int idx{};
+    std::vector<int> answer_vec(nums.size(), 0);
+
+    for (int i{}; i < nums.size(); i++)
+    {
+        if (nums[i] == 0)
+        {
+            idx = i;
+            count++;
+        }
+    }
+    if (count == 1)
+    {
+        for (int i{}; i < nums.size(); i++)
+        {
+            if (i == idx) i++;
+            accum = nums[i];
+        }
+        answer_vec[idx] = accum;
+    }
+    else if (!count)
+    {
+        for (auto e : nums)
+        {
+            accum = e;
+        }
+        answer_vec.clear();
+        for (auto e : nums)
+        {
+            answer_vec.push_back(accum / e);
+        }
+    }
+    return answer_vec;
+}
+
+std::vector<int> ProductExceptSelf2(const std::vector<int> &nums)
+{
+    int multiply_sum{ 1 };
+    int zeroCount{};
+    for (auto num : nums)
+    {
+        if (num != 0)
+        {
+            multiply_sum *= num;
+        }
+        else
+        {
+            zeroCount++;
+        }
+    }
+
+    std::vector<int> answer;
+
+    for (auto num : nums)
+    {
+        if (zeroCount != 0)
+        {
+            if (num == 0 && zeroCount == 1)
+            {
+                answer.push_back(multiply_sum);
+            }
+            else
+            {
+                answer.push_back(0);
+            }
+        }
+        else
+        {
+            answer.push_back(multiply_sum / num);
+        }
+    }
+
+    return answer;
+}
+
 int main()
 {
     std::vector<int> nums{ -1,1,0,-3,3 };
